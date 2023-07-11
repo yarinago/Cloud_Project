185e1c5c0bd241bda529f3d744473a33ec374b91
yarinago <yarinago151295@gmail.com>
2023-07-11 18:29:02 +0300

    Create dockerfile, docker-compose and dockerignore files.
    Create an backservice_test.py file inside tests folder. This is the file that will do the unit-testing for the API request

---
+++ tests/backservice_test.py
@@ -0,0 +1,13 @@
+""" GET:
+        - Return All
+        - Return One
+        - Return Many
+        - None existing
+        - Params not valid
+    
+    POST:
+        - Insert for One & Many
+        - Update for One & Many
+        - None existing
+        - Params not valid
+"""
