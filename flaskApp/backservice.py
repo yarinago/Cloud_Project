76108af25a4c45d42bafdfe4c8d620d8db0d15ee
yarinago <67272021+yarinago@users.noreply.github.com>
2023-08-02 18:06:44 +0300

    Update backservice.py

--- flaskApp/backservice.py
+++ flaskApp/backservice.py
@@ -11,7 +11,6 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-dbConnectionOptions = "-c statement_timeout={}".format(os.getenv("DB_TIMEOUT"))
 connection = object
 # endregion
 # region SQL QUERIES PARAMS
